ac4cd344db6fc5eb76894716739a9eb9ea426f6a
Durdica Stancl <durdicastancl7@gmail.com>
2023-08-03 08:50:32 +0200

    Change code for fetching data example

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+(async function() {
+
 const source = document.getElementById("entry-template").innerHTML;
 const template = Handlebars.compile(source);
 
-const context = { title: "My New Post", body: "This is my first post!", imgURL:"https://robohash.org/matej" };
+const response = await fetch("https://pokeapi.co/api/v2/pokemon-color/yellow")
+
+const data = await response.json()
+
+const pokemons = data.pokemon_species.slice(0, 20)
+console.log(pokemons)
+
+const context = { pokemons };
 const html = template(context);
 
-//console.log(html);
-//document.getElementsByName("body")[0].innerHTML = html;
 document.querySelector("body").innerHTML = html
-console.log(html);
+
+})()
 
 // MENI OVO NE RADI KAO MATEJU
 
