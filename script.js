0d8e2cc8da9f643febe1adc22c0127a7bbe03462
Durdica Stancl <durdicastancl7@gmail.com>
2023-08-03 19:26:40 +0200

    Add jquery example

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-(async function() {
 
-const source = document.getElementById("entry-template").innerHTML;
-const template = Handlebars.compile(source);
-
-const response = await fetch("https://pokeapi.co/api/v2/pokemon-color/yellow")
-
-const data = await response.json()
-
-const pokemons = data.pokemon_species.slice(0, 20)
-console.log(pokemons)
-
-const context = { pokemons };
-const html = template(context);
-
-document.querySelector("body").innerHTML = html
-
-})()
-
-// MENI OVO NE RADI KAO MATEJU
-
-// Stranica: robohash.org, dosta zanimljiv API
+$(document).ready(function() {
+    $.get("https://pokeapi.co/api/v2/pokemon-color/yellow")
+      .done(render)
+      .fail(function(error) {
+        console.log(error);
+    });
+  
+    $("#green").click(function() {
+      $.get("https://pokeapi.co/api/v2/pokemon-color/green")
+      .done(render)
+      .fail(function(error) {
+        console.log(error);
+      });
+    })
+  
+    $("#yellow").click(function() {
+      $.get("https://pokeapi.co/api/v2/pokemon-color/yellow")
+      .done(render)
+      .fail(function(error) {
+        console.log(error);
+      });
+    })
+  
+    $("#blue").click(function() {
+      $.get("https://pokeapi.co/api/v2/pokemon-color/blue")
+      .done(render)
+      .fail(function(error) {
+        console.log(error);
+      });
+    })
+  
+    function render(data) {
+      const source = $("#pokemon-list").html();
+        const template = Handlebars.compile(source);
+        const context = {
+          pokemon: data.pokemon_species.slice(0, 20),
+          title: "Pokemons",
+        };
+        const html = template(context);
+  
+        $("#result").html(html);
+    }
+  
+  })
